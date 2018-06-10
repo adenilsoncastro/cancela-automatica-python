3065a41bb7c9b41a74e0855c5000ad7ca6436a05
oluis <o.luisfernando@outlook.com>
2018-06-10 17:08:06 -0300

    aa

--- platews.py
+++ platews.py
@@ -1,9 +1,14 @@
 import requests
 import json
+import base64
 
 def checkForPlateExistence(plate):
+
+    with open(r"C:\Users\oluis\Desktop\TCC\cancela-automatica-python\gol51_threshold.png", "rb") as image_file:
+        img = base64.b64encode(image_file.read())
+
     url = 'http://localhost:8080/plates/checkforexistence'
-    data = {'plate': plate }
+    data = {'plate': plate, 'img': img }
     response = requests.post(url, data=data)
 
     obj = json.loads(response.text)
@@ -11,3 +16,5 @@ def checkForPlateExistence(plate):
         
 
     return response
+
+checkForPlateExistence('ayh-2598')
